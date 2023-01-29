2e4297af12614a58855b132fb49cd4df88a7b576
Jeffrey <zh28@rice.edu>
2023-01-29 15:03:48 -0600

    Across/Hop differnce test

--- cross-chain-bridges/src/processor.ts
+++ cross-chain-bridges/src/processor.ts
@@ -1,11 +1,18 @@
-import { BigNumber } from 'bignumber.js/bignumber';
-import { chain, token } from '@sentio/sdk/lib/utils';
-import { MultichainRouterContext, MultichainRouterProcessor, LogAnySwapOutEvent } from './types/multichainrouter';
-import { CBridgeContext, CBridgeProcessor, SendEvent } from './types/cbridge';
-import { HopBridgeContext, HopBridgeProcessor, TransferSentEvent } from './types/hopbridge';
-import { HopBridgeEthereumContext, HopBridgeEthereumProcessor, TransferSentToL2Event } from './types/hopbridgeethereum';
-import { StargatePoolContext, StargatePoolProcessor, SwapEvent } from './types/stargatepool';
-import { MultichainMap, CBridgeMap, HopMap, StargateMap } from './addresses';
+import { BigNumber } from "bignumber.js/bignumber";
+import { chain, token } from "@sentio/sdk/lib/utils";
+import { MultichainRouterContext, MultichainRouterProcessor, LogAnySwapOutEvent, LogAnySwapInEvent } from "./types/multichainrouter";
+import { CBridgeContext, CBridgeProcessor, SendEvent, RelayEvent } from "./types/cbridge";
+import {
+  HopBridgeContext, HopBridgeProcessor, TransferSentEvent,
+  WithdrawalBondedEvent as WithdrawalBondedL2Event
+} from "./types/hopbridge";
+import {
+  HopBridgeEthereumContext, HopBridgeEthereumProcessor, TransferSentToL2Event,
+  WithdrawalBondedEvent as WithdrawalBondedEthereumEvent
+} from "./types/hopbridgeethereum";
+import { StargatePoolContext, StargatePoolProcessor, SwapEvent, SwapRemoteEvent } from "./types/stargatepool";
+import { AcrossToContext, AcrossToProcessor, FundsDepositedEvent, FilledRelayEvent } from "./types/acrossto";
+import { MultichainMap, CBridgeMap, HopMap, StargateMap, AcrossMap } from "./addresses";
 
 const EthPrice = 1200
 
@@ -16,97 +23,50 @@ const mapOrder = function (value: BigNumber): string {
   else return "large (>$3k)";
 }
 
-// ================================= Multichain =================================
-const handleSwapOutMultichain = function (chainId: string, tokenName: string, decimal: number) {
-  const chainName = chain.getChainName(chainId).toLowerCase()
-  return async function (event: LogAnySwapOutEvent, ctx: MultichainRouterContext) {
-    var value = token.scaleDown(event.args.amount, decimal)
-    if (tokenName == 'ETH') value = value.multipliedBy(EthPrice)
-    const toChain = chain.getChainName(event.args.toChainID.toString()).toLowerCase()
-    ctx.meter.Gauge('swapOutAmount').record(value, {
-      "to": toChain,
-      "loc": chainName,
-      "token": tokenName,
-      "bridge": 'Multichain',
-    })
-    ctx.meter.Gauge('swapOutType').record(1, {
-      "type": mapOrder(value),
-      "to": toChain,
-      "loc": chainName,
-      "token": tokenName,
-      "bridge": 'Multichain',
-    })
+const generateLabel = function (
+  chainName: string,
+  tokenName: string,
+  bridge: string,
+  isOut: boolean
+): { [index: string]: string } {
+  if (isOut) return {
+    "src": chainName,
+    "token": tokenName,
+    "bridge": bridge,
+  }; else return {
+    "dst": chainName,
+    "token": tokenName,
+    "bridge": bridge,
   }
 }
 
-for (const [chainId, [routerList, tokenList]] of Object.entries(MultichainMap)) {
-  for (var [tokenName, tokenAddr, decimal] of tokenList) {
-    for (var routerAddress of routerList) {
-      MultichainRouterProcessor
-        .bind({ address: routerAddress, network: Number(chainId) })
-        .onEventLogAnySwapOut(
-          handleSwapOutMultichain(chainId, tokenName, decimal),
-          MultichainRouterProcessor.filters.LogAnySwapOut(tokenAddr)
-        )
-    }
-  }
-}
-
-// ================================= CBridge =================================
-const handleSwapOutCBridge = function (chainId: string, tokenName: string, decimal: number, tokenAddr: string) {
-  const chainName = chain.getChainName(chainId).toLowerCase()
-  return async function (event: SendEvent, ctx: CBridgeContext) {
-    var value = token.scaleDown(event.args.amount, decimal)
-    if (tokenName == 'ETH') value = value.multipliedBy(EthPrice)
-    const toChain = chain.getChainName(event.args.dstChainId.toString()).toLowerCase()
-    if (event.args.token == tokenAddr) {
-      ctx.meter.Gauge('swapOutAmount').record(value, {
-        "to": toChain,
-        "loc": chainName,
-        "token": tokenName,
-        "bridge": 'CBridge',
-      })
-      ctx.meter.Gauge('swapOutType').record(1, {
-        "type": mapOrder(value),
-        "to": toChain,
-        "loc": chainName,
-        "token": tokenName,
-        "bridge": 'CBridge',
-      })
-    }
-  }
-}
-
-for (const [chainId, [cBridgeAddress, tokenList]] of Object.entries(CBridgeMap)) {
-  for (const [tokenName, tokenAddr, decimal] of tokenList) {
-    CBridgeProcessor.bind({ address: cBridgeAddress, network: Number(chainId) })
-      .onEventSend(handleSwapOutCBridge(chainId, tokenName, decimal, tokenAddr))
-  }
-}
 
 // ================================= Hop =================================
 const handleSwapOutHop = function (chainId: string, tokenName: string, decimal: number) {
   const chainName = chain.getChainName(chainId).toLowerCase()
+  const labelAmount = generateLabel(chainName, tokenName, "Hop", true)
   return async function (
     event: TransferSentEvent | TransferSentToL2Event,
     ctx: HopBridgeContext | HopBridgeEthereumContext
   ) {
     var value = token.scaleDown(event.args.amount, decimal)
-    if (tokenName == 'ETH') value = value.multipliedBy(EthPrice)
-    const toChain = chain.getChainName(event.args.chainId.toString()).toLowerCase()
-    ctx.meter.Gauge('swapOutAmount').record(value, {
-      "to": toChain,
-      "loc": chainName,
-      "token": tokenName,
-      "bridge": 'Hop',
-    })
-    ctx.meter.Gauge('swapOutType').record(1, {
-      "type": mapOrder(value),
-      "to": toChain,
-      "loc": chainName,
-      "token": tokenName,
-      "bridge": 'Hop',
-    })
+    if (tokenName == "ETH") value = value.multipliedBy(EthPrice)
+    ctx.meter.Gauge("swapOutAmount").record(value, labelAmount)
+    ctx.meter.Gauge("swapOutType").record(1, {...labelAmount, "type": mapOrder(value)})
+  }
+}
+
+const handleSwapInHop = function (chainId: string, tokenName: string, decimal: number) {
+  const chainName = chain.getChainName(chainId).toLowerCase()
+  const labelAmount = generateLabel(chainName, tokenName, "Hop", false)
+  return async function (
+    event: WithdrawalBondedEthereumEvent | WithdrawalBondedL2Event,
+    ctx: HopBridgeContext | HopBridgeEthereumContext
+  ) {
+    var value = token.scaleDown(event.args.amount, decimal)
+    if (tokenName == "ETH") value = value.multipliedBy(EthPrice)
+    ctx.meter.Gauge("swapInAmount").record(value, labelAmount)
+    ctx.meter.Gauge("swapInType").record(1, {...labelAmount, "type": mapOrder(value)})
   }
 }
 
@@ -115,46 +75,52 @@ for (const [chainId, tokenList] of Object.entries(HopMap)) {
     for (const [tokenName, tokenAddr, decimal] of tokenList) {
       HopBridgeEthereumProcessor.bind({ address: tokenAddr, network: Number(chainId) })
         .onEventTransferSentToL2(handleSwapOutHop(chainId, tokenName, decimal))
+        .onEventWithdrawalBonded(handleSwapInHop(chainId, tokenName, decimal))
     }
   } else {
     for (const [tokenName, tokenAddr, decimal] of tokenList) {
       HopBridgeProcessor.bind({ address: tokenAddr, network: Number(chainId) })
         .onEventTransferSent(handleSwapOutHop(chainId, tokenName, decimal))
+        .onEventWithdrawalBonded(handleSwapInHop(chainId, tokenName, decimal))
     }
   }
 }
 
-// ================================= Stargate =================================
-const StargateChainIdMap: { [index: number]: number } = {
-  1: 1, 2: 56, 6: 43114, 9: 137, 10: 42161, 11: 10, 12: 250,
-  101: 1, 102: 56, 106: 43114, 109: 137, 110: 42161, 111: 10, 112: 250,
-}
 
-const handleSwapOutStargate = function (chainId: string, tokenName: string) {
+// ================================= AcrossTo =================================
+const handleSwapOutAcross = function (chainId: string, tokenName: string, decimal: number) {
   const chainName = chain.getChainName(chainId).toLowerCase()
-  return async function (event: SwapEvent, ctx: StargatePoolContext) {
-    var value = token.scaleDown(event.args.amountSD, 6)
-    if (tokenName == 'ETH') value = value.multipliedBy(EthPrice).dividedBy(1000000000000)
-    const toChain = chain.getChainName(StargateChainIdMap[event.args.chainId]).toLowerCase()
-    ctx.meter.Gauge('swapOutAmount').record(value, {
-      "to": toChain,
-      "loc": chainName,
-      "token": tokenName,
-      "bridge": 'Stargate',
-    })
-    ctx.meter.Gauge('swapOutType').record(1, {
-      "type": mapOrder(value),
-      "to": toChain,
-      "loc": chainName,
-      "token": tokenName,
-      "bridge": 'Stargate',
-    })
+  const labelAmount = generateLabel(chainName, tokenName, "AcrossTo", true)
+  return async function (event: FundsDepositedEvent, ctx: AcrossToContext) {
+    var value = token.scaleDown(event.args.amount, decimal)
+    if (tokenName == "ETH") value = value.multipliedBy(EthPrice)
+    ctx.meter.Gauge("swapOutAmount").record(value, labelAmount)
+    ctx.meter.Gauge("swapOutType").record(1, {...labelAmount, "type": mapOrder(value)})
   }
 }
 
-for (const [chainId, tokenList] of Object.entries(StargateMap)) {
-  for (const [tokenName, poolAddress, poolID] of tokenList) {
-    StargatePoolProcessor.bind({ address: poolAddress, network: Number(chainId) })
-      .onEventSwap(handleSwapOutStargate(chainId.toString(), tokenName))
+const handleSwapInAcross = function (chainId: string, tokenName: string, decimal: number, tokenAddr: string) {
+  const chainName = chain.getChainName(chainId).toLowerCase()
+  const labelAmount = generateLabel(chainName, tokenName, "AcrossTo", false)
+  return async function (event: FilledRelayEvent, ctx: AcrossToContext) {
+    var value = token.scaleDown(event.args.amount, decimal)
+    if (tokenName == "ETH") value = value.multipliedBy(EthPrice)
+    if (event.args.destinationToken == tokenAddr) {
+      ctx.meter.Gauge("swapInAmount").record(value, labelAmount)
+      ctx.meter.Gauge("swapInType").record(1, {...labelAmount, "type": mapOrder(value)})
+    }
+  }
+}
+
+for (const [chainId, [poolAddr, tokenList]] of Object.entries(AcrossMap)) {
+  for (const [tokenName, tokenAddr, decimal] of tokenList) {
+    AcrossToProcessor.bind({ address: poolAddr, network: Number(chainId) })
+      .onEventFundsDeposited(
+        handleSwapOutAcross(chainId, tokenName, decimal),
+        AcrossToProcessor.filters.FundsDeposited(null, null, null, null, null, null, tokenAddr)
+      )
+      .onEventFilledRelay(
+        handleSwapInAcross(chainId, tokenName, decimal, tokenAddr)
+      )
   }
 }
