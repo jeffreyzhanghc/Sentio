d8237f25db9c719ba43e342915014fed5458e50a
Jeffrey <zh28@rice.edu>
2023-02-06 09:27:03 -0600

    synapse changes

--- synapse/src/processor.ts
+++ synapse/src/processor.ts
@@ -1,6 +1,6 @@
 import { scaleDown } from '@sentio/sdk/lib/utils/token'
 import { token,chain } from '@sentio/sdk/lib/utils'
-import { SynapseProcessor, SynapseContext,TokenDepositAndSwapEvent,TokenRedeemAndSwapEvent,TokenRedeemAndRemoveEvent} from './types/synapse'
+import { SynapseProcessor, SynapseContext,TokenDepositAndSwapEvent,TokenRedeemAndSwapEvent} from './types/synapse'
 import {
     Bridge, 
     Tokens, 
@@ -11,12 +11,12 @@ import {
 
 
 const Map: { [index: number]: [string, [number,string, string, number][]] } = {
-  1: ["0x2796317b0fF8538F253012862c06787Adfb8cEb6",[
+  1: ["0x6571d6be3d8460CF5F7d6711Cd9961860029D85F",[
     [1,"USDC", "0x7ea2be2df7ba6e54b1a9c70676f668455e329d29", 18],
     [2,"USDT","0x22648c12acd87912ea1710357b1302c6a4154ebc", 18],
   ]],     // ethereum
 
-  10: ["0xAf41a65F786339e7911F4acDAD6BD49426F2Dc6b",[
+  10: ["0x470f9522ff620eE45DF86C58E54E6A645fE3b4A7",[
     [1,"USDC", "0xf390830DF829cf22c53c8840554B98eafC5dCBc2", 18],
     [2,"USDT", "0x86B3F23B6e90F5bbfac59b5b2661134Ef8Ffd255", 18],
   ]],     // optimism
@@ -26,22 +26,22 @@ const Map: { [index: number]: [string, [number,string, string, number][]] } = {
     [2,"USDT", "0xE3eeDa11f06a656FcAee19de663E84C7e61d3Cac", 18],
   ]],     // polygon
 
-  42161: ["0x6F4e8eBa4D337f874Ab57478AcC2Cb5BACdc19c9",[
+  42161: ["0xd1F359B743073AD00B1B45909A6aB13Ac9149401",[
     [1,"USDC", "0x3405A1bd46B85c5C029483FbECf2F3E611026e45", 18],
     [2,"USDT", "0x05e481B19129B560E921E487AdB281E70Bdba463", 18],
   ]],     // arbitrum
 
-  43114: ["0xC05e61d0E7a63D27546389B7aD62FdFf5A91aACE",[
+  43114: ["0x0EF812f4c68DC84c22A4821EF30ba2ffAB9C2f3A",[
     [1,"USDC", "0xcc9b1F919282c255eB9AD2C0757E8036165e0cAd", 18],
     [2,"USDT", "0x94977c9888F3D2FAfae290d33fAB4a5a598AD764", 18],
   ]],     // avalanche
 
-  250:["0xAf41a65F786339e7911F4acDAD6BD49426F2Dc6b",[
+  250:["0xB003e75f7E0B5365e814302192E99b4EE08c0DEd",[
     [1,"USDC", '0x95bf7E307BC1ab0BA38ae10fc27084bC36FcD605', 18],
     [2,"USDT", '0x2823D10DA533d9Ee873FEd7B16f4A962B2B7f181', 18],
   ]], //fantom
 
-  56:["0xd123f70AE324d34A9E76b67a27bf77593bA8749f",[
+  56:["0x749F37Df06A99D6A8E065dd065f8cF947ca23697",[
     [1,"USDC", '0x8965349fb649a33a30cbfda057d8ec2c48abe2a2', 18],
     [2,"USDT", '0xedf0c420bc3b92b961c6ec411cc810ca81f5f21a', 18],
   ]] //binance
